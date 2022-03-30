508a7ae53721599db16144d5a8a26af6597b5df3
Dennis Stoy <docs1887@googlemail.com>
2022-03-30 21:00:02 +0200

    GUI and calculation added. But Bugs inside

--- main.py
+++ main.py
@@ -1,58 +1,70 @@
 import tkinter as tk
 
+
 app = tk.Tk()
+app.geometry("150x220+50+50")
+
+gui_items = list()
+button_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-',
+                 '*', '/', '=', 'AC']
+
+calculation = str()
 
 
-button_one = tk.Button(app,text="1")
-button_two = tk.Button(app,text="2")
-button_three = tk.Button(app,text="3")
-button_four = tk.Button(app,text="4")
-button_five = tk.Button(app,text="5")
-button_six = tk.Button(app,text="6")
-button_seven = tk.Button(app,text="7")
-button_eight = tk.Button(app,text="8")
-button_nine = tk.Button(app,text="9")
-button_equal = tk.Button(app, text="=")
-button_plus = tk.Button(app, text="+")
-button_minus = tk.Button(app, text="-")
-button_mal = tk.Button(app, text="*")
-button_share = tk.Button(app, text=":")
-button_comma = tk.Button(app, text=".")
-button_zero = tk.Button(app, text="0")
-button_double_zero = tk.Button(app, text="00")
-button_plus_minus = tk.Button(app, text="+/-")
+def add_button_value_to_calculation(value):
+    global calculation
+    calculation = calculation + value
+    output_label['text'] = calculation
 
-button_clear = tk.Button(app, text="C")
+    if value == 'AC':
+        calculation = str()
+        output_label['text'] = '...'
+        return
 
-button_one.place(x=20, y=200, width=50, height=50)
-button_two.place(x=75, y=200, width=50, height=50)
-button_three.place(x=130, y=200, width=50, height=50)
-button_plus_minus.place(x=185, y=200, width=50, height=50)
-button_clear.place(x=240, y=200, width=50, height=50)
+    if value == '=':
+        calculate(calculation)
+        calculation = str()
+        return
 
-button_four.place(x=20, y=255, width=50, height=50)
-button_five.place(x=75, y=255, width=50, height=50)
-button_six.place(x=130, y=255, width=50, height=50)
-
-button_seven.place(x=20, y=310, width=50, height=50)
-button_eight.place(x=75, y=310, width=50, height=50)
-button_nine.place(x=130, y=310, width=50, height=50)
-
-button_comma.place(x=20, y=365, width=50, height=50)
-button_double_zero.place(x=130, y=365, width=50, height=50)
-button_zero.place(x=75, y=365, width=50, height=50)
-button_plus_minus.place(x=185, y=200, width=50, height=50)
-
-button_equal.place(x=240, y=365, width=50, height=50)
-button_plus.place(x=185, y=310, width=50, height=105)
-button_minus.place(x=185, y=255, width=50, height=50)
-button_mal.place(x=240, y=255, width=50, height=50)
-button_share.place(x=240, y=310, width=50, height=50)
+    calculation = calculation + value
+    output_label['text'] = calculation
 
 
+def calculate(calc):
+    try:
+        result = eval(calc)
+        print(result)
+        output_label['text'] = result
+
+    except Exception as e:
+        print(e)
+        output_label['text'] = "Error!"
 
 
-if __name__ == "__main__":
-    app.title("Taschenrechner 2000")
-    app.geometry("310x450+50+50")
+def create_button(value):
+    button = tk.Button(
+        text=value, command=lambda: add_button_value_to_calculation(value))
+    gui_items.append(button)
+
+
+for val in button_values:
+    create_button(val)
+
+output_label = tk.Label(text="Hallo")
+output_label.grid(row=0, columnspan=10)
+
+column_count = 0
+row_count = 0
+max_columns = 3
+
+for item in gui_items:
+    item.grid(row=row_count, column=column_count)
+    column_count += 1
+
+    if column_count == max_columns:
+        column_count = 0
+        row_count += 1
+
+
+if __name__ == '__main__':
     app.mainloop()
